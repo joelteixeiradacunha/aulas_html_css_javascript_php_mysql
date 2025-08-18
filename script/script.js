20644c13a2a231e97110340f6d4c2789da277d2c
Joel T Cunha <81336449+joeltcunha@users.noreply.github.com>
2025-08-18 15:33:58 -0300

    ferrei os alunos

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// var - deixou de ser recomendado, mas ainda é usado
+// let - escopo de bloco, recomendado para variáveis que mudam
+// const - escopo de bloco, recomendado para constantes
+
+
+var nome = 'João'; // variável global
+
+let anoNascimento = 1968;
+
+let x = 5;
+
+x = x + 5;
+
+print(x)
